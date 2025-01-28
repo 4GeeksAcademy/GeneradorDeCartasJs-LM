04e9dc9af3a787485aaa172cbe0f383c50731209
Leodelis <leodelis19@gmail.com>
2025-01-28 22:09:26 +0000

    GeneradorDeCarta+button+temporizador

--- src/app.js
+++ src/app.js
@@ -6,6 +6,66 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  showRandomCard();
+
+  setInterval(showRandomCard, 10000);
 };
+
+// Función para generar carta aleatoria
+
+function getRandomCard() {
+  const numbers = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const symbols = ["♦", "♥", "♠", "♣"];
+
+  // Index aleatorio de número y simbolo
+  const randomNumberIndex = Math.floor(Math.random() * numbers.length);
+  const randomSymbolIndex = Math.floor(Math.random() * symbols.length);
+
+  // Condición ternario con los index del array symbols
+  const randomSymbolColor = randomSymbolIndex <= 1 ? "red" : "black";
+
+  // haciendo el return como un objeto
+  return {
+    symbol: symbols[randomSymbolIndex],
+    color: randomSymbolColor,
+    number: numbers[randomNumberIndex]
+  };
+}
+
+// Botón para generar una nueva carta
+
+function showRandomCard() {
+  // obtengo una carta aleatoria
+  const card = getRandomCard();
+
+  // obtengo los elementos donde establecer los atributos de la carta
+  const cardSymbolTopElement = document.getElementById("card-symbol-top");
+  const cardNumber = document.getElementById("card-number");
+  const cardSymbolBottomElement = document.getElementById("card-symbol-bottom");
+
+  // establezco el contenido de los elementos con el valor de los atributos de la carta
+  cardSymbolTopElement.innerText = card.symbol;
+  cardSymbolTopElement.style.color = card.color;
+  cardNumber.innerText = card.number;
+  cardSymbolBottomElement.innerText = card.symbol;
+  cardSymbolBottomElement.style.color = card.color;
+
+  // escuchar evento onclick del botón
+  const newCardButton = document.getElementById("get-new-card");
+  newCardButton.onclick = showRandomCard;
+}
